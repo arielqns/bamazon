0bdb96e2e90faa57ad0c990dc7dd9a4fb4ef937e
Ariel Quinones <arielqns@gmail.com>
2019-10-13 16:47:45 -0400

    completed readme

--- bamazonManager.js
+++ bamazonManager.js
@@ -56,6 +56,7 @@ function showItems() {
     connection.query("SELECT * FROM products", function (err, results) {
         console.log("");
         for (var i = 0; i < results.length; i++) {
+            console.log(
                 results[i].itemid +
                 " | " + results[i].productname +
                 " | " + results[i].departmentname +
